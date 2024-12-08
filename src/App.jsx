37e855f2a16b04dc3ad82d8a2a7c72b883ac2003
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-12-08 23:05:02 +0530

    added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <h1 className='bg-red-500 text-3xl font-bold underline'>
-      Hello World
-    </h1>
+
+    <Navbar />
   )
 }
 
