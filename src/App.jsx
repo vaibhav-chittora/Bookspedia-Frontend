02cb6e637f1905f08b34e329eeae357e8704aa7b
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-12-09 00:01:19 +0530

    added banner and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-
-    <Navbar />
+    <>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </>
   )
 }
 
